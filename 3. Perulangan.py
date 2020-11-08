4386b754530a60f32fa9532047c84a3fed015d7a
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 20:33:51 +0700

    Memahami perulangan

--- 3. Perulangan.py
+++ 3. Perulangan.py
@@ -1,2 +1,17 @@
 #Perulangan
 
+jumlah_anak = 4
+
+for index_anak in range(1, jumlah_anak+1): #Jumlah Perulangan = 5 - 1 = 4
+    print(f'Halo Anak #{index_anak}')
+
+
+#code dibawah hanya garis
+print('*' *50)
+
+jumlah_mobil = 3
+
+for index_mobil in range(1, jumlah_mobil+1):
+    print(f'Absen Mobil #{index_mobil}')
+
+
