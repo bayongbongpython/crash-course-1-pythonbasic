bc41d9a9e81e45e63b1479fc29c159abaea84d46
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 23:07:50 +0700

    Tipe data dictionary 2

---
+++ 5. tipe Data Dictionary Complex.py
@@ -0,0 +1,8 @@
+print('Data ini dikirimkan oleh Server Gojek, untuk memberikan informasi driver disekitar pemakai aplikasi')
+data_dari_dari_server_Gojek = {
+    'tanggal': '2020-06-10'
+    'driver_list':  'Eko', 'Dwi', 'Tri', 'Catur'
+}
+
+print(data_dari_Server_Gojek)
+print(f"Driver disekitar sini {data_dari_dari_server_Gojek['driver_list']}")
