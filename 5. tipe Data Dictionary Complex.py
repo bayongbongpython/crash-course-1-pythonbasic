7dd8787667465875700a701b2ebddbc6734ab281
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 23:53:16 +0700

    Tipe data dictionary complex

--- 5. tipe Data Dictionary Complex.py
+++ 5. tipe Data Dictionary Complex.py
@@ -1,8 +1,9 @@
-print('Data ini dikirimkan oleh Server Gojek, untuk memberikan informasi driver disekitar pemakai aplikasi')
-data_dari_dari_server_Gojek = {
-    'tanggal': '2020-06-10'
-    'driver_list':  'Eko', 'Dwi', 'Tri', 'Catur'
+print('data ini dikirimkan oleh server gojek, untuk memberikan informasi driver di sekitar pemakai aplikasi')
+data_dari_server_gojek = {
+    'tanggal': '2020-06-10',
+    'driver_list': ['Eko', 'Dwi', 'Tri', 'Catur']
 }
 
-print(data_dari_Server_Gojek)
-print(f"Driver disekitar sini {data_dari_dari_server_Gojek['driver_list']}")
+print(data_dari_server_gojek)
+print(f"Driver disekitar sini {data_dari_server_gojek['driver_list']}")
+
