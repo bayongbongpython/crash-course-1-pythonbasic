5d523148218c26db74e430fa3858dc162e658a6d
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 22:27:33 +0700

    Tipe data dictionary

--- 4. Tipe Data Dictionary.py
+++ 4. Tipe Data Dictionary.py
@@ -4,6 +4,29 @@ KVP = Key Value Pair
 dictionary = kamus
 """
 
+kamus = {}
+kamus['anak laki laki'] = 'son'
+kamus['anak perempuan'] = 'daughter'
+kamus['istri'] = 'wife'
+kamus['ayah'] = 'husband'
+kamus['ibu'] = 'mother'
 
+print(kamus)
+print(kamus['anak laki laki'])
+print(kamus['anak perempuan'])
+print(kamus['istri'])
+print(kamus['ayah'])
+print(kamus['ibu'])
+
+#atau dengan penulisan
+
+kamus = {'anak laki laki': 'son', 'anak perempuan': 'daughter', 'istri': 'wife', 'ayah': 'husband', 'ibu': 'mother'}
+
+print(kamus)
+print(kamus['anak laki laki'])
+print(kamus['anak perempuan'])
+print(kamus['istri'])
+print(kamus['ayah'])
+print(kamus['ibu'])
 
 
