aa58f130c9ea24f92aef635a134d1f279005748b
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 20:06:51 +0700

    Percabangan adalah. Check code

---
+++ 2. Percabangan.py
@@ -0,0 +1,20 @@
+print('Percabangan')
+
+#Percabangan atau eksekusi terpilih menerangkan dua pilihan yang berlainan yang harus dipilih salah satunya
+# ditandai dengan jika (IF, ELSE).
+
+ingin_cepat = True
+if ingin_cepat:
+    print('Jalan lurus aja ya!')
+else:
+    print('Jalan lain')
+
+#baris code dibawah ini hanya garis
+print('-' * 50 )
+
+ingin_cepat = False
+if ingin_cepat:
+    print('Jakan lurus aja ya!')
+else:
+    print('Jalan lain')
+
