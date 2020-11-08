a8a89a4a0b893617fe1ec414c4ae7cd057314df2
WORK <andi.hidayat.mail@gmail.com>
2020-11-08 18:51:05 +0700

    This file show you how to create single comment and multiple comment

--- Comment.py
+++ Comment.py
@@ -1,10 +1,13 @@
 print('to see what written in comment line please open this python file using python ide ')
+
 #This is Singgle Comment. This sentence will not visible because just a comment.
 #Single comment marked with tagar (#)
 
 #This is multiple comment
 '''
 Multiple comment marked with double triple upper ,,,
-What ever you erite here will not be vissible
+What ever you write here will not be vissible
 even you beg python interpreter to show it. 
 '''
+
+#After you run this program you just only be able to se first line sententce above
