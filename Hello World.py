9bf3701e31a71f6e66cc464b9e1731dcaafe7b1a
Andi Hidayat <73642859+andihidayat12@users.noreply.github.com>
2020-11-08 18:35:48 +0700

    Greeting

---
+++ Hello World.py
@@ -0,0 +1,7 @@
+print('Hello World')
+print('Let me introduce myself')
+print('My name is Andi Hidayat')
+print('I am 33 years old')
+print('I live in Garut')
+print('i love to do with Adobe Illustrator and do coding with Python')
+print('Thank you')
