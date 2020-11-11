db1bc8737b68450aa6efd04d07c11f4025a59ddb
WORK <andi.hidayat.mail@gmail.com>
2020-11-11 09:23:32 +0700

    Macam macam Tipe Data List yang penting untuk di ketahui

--- 8. Type data list penting.py
+++ 8. Type data list penting.py
@@ -1,4 +1,4 @@
-#Type data Skalar = Tipe data sederhana
+print('Type Data Skalar atau Tipe data sederhana')
 anak1 = 'Eko'
 anak2 = 'Dwi'
 anak3 = 'Tri'
@@ -9,11 +9,17 @@ print(anak2)
 print(anak3)
 print(anak4)
 
-#Type data list/daftar/array
+print('\nType data list/daftar/array')
 anak = ['Eko', 'Dwi', 'Tri', 'Catur']
 print(anak)
 anak.append('Panca')
 print(anak)
 
-# Sapa anak ke-2
+print('\nSapa anak ke-2')
 print(f'Hai {anak[1]}!')
+
+print('\nSapa Semua Anak')
+for a in anak:
+    print(f'Hai {a}!')
+
+
